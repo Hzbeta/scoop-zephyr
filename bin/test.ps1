22d0b94ca026a48a7589f2f951acb10edf1b38c1
Hzbeta <hzbeta@yahoo.com>
2024-11-28 12:33:12 +0800

    🐛 fix(test): delete non-manifest JSON files before running tests

--- bin/test.ps1
+++ bin/test.ps1
@@ -2,6 +2,18 @@
 #Requires -Modules @{ ModuleName = 'BuildHelpers'; ModuleVersion = '2.0.1' }
 #Requires -Modules @{ ModuleName = 'Pester'; ModuleVersion = '5.2.0' }
 
+# Delete non-manifest JSON files before testing
+# This prevents test from running schema validation on non-manifest JSON files
+$jsonToDelete = @(
+    '.markdownlint.json'
+)
+$jsonRoot = "$PSScriptRoot/.."
+foreach ($json in $jsonToDelete) {
+    if (Test-Path Join-Path $jsonRoot $json) {
+        Remove-Item -Path Join-Path $jsonRoot $json -Force
+    }
+}
+
 $pesterConfig = New-PesterConfiguration -Hashtable @{
     Run    = @{
         Path     = "$PSScriptRoot/.."
